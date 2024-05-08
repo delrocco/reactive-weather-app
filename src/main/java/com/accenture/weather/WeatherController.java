1f1e61141c1682fe7af38f886145a71b68ba31e8
delrocco <delrocco@users.noreply.github.com>
2024-05-07 21:55:24 -0400

    remove static view; default-content-type is json

--- src/main/java/com/accenture/weather/WeatherController.java
+++ src/main/java/com/accenture/weather/WeatherController.java
@@ -1,37 +1,28 @@
 package com.accenture.weather;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
-import java.time.LocalDate;
 
-@Controller
+@RestController
+@RequestMapping("/weather")
 public class WeatherController
 {
     @Autowired
     private WeatherService svcWeather;
 
-    @GetMapping({"/", "/weather"})
-    public String weather(
-            @RequestParam(name="ahead", defaultValue="1") int ahead,
-            @RequestParam(name="night", defaultValue="false") boolean night,
-            Model model)
+    @GetMapping
+    public Mono<WeatherModel> getForecastToday()
     {
-        WeatherModel modelWeather = svcWeather.getForecastOfDay(ahead, night);
-
-        model.addAttribute("date", modelWeather.get_date());
-        model.addAttribute("day_name", modelWeather.get_day_name());
-        model.addAttribute("temp_high_celsius", modelWeather.get_temp_high_celsius());
-        model.addAttribute("forecast_blurb", modelWeather.get_forecast_blurb());
-
-        return "weather";
+        return svcWeather.getForecastOfDay("1", "false");
     }
 
-    private String getValidDate()
+    @GetMapping("/{dayidx}")
+    public Mono<WeatherModel> getForecastOfDay(
+            @PathVariable String dayidx,
+            @RequestParam(name="night", defaultValue="false") String night)
     {
-        return null;
+        return svcWeather.getForecastOfDay(dayidx, night);
     }
 }
