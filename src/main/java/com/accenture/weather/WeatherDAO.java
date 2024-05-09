e6a0a4cf55f99182a2906a04194c38c723d606d4
delrocco <delrocco@users.noreply.github.com>
2024-05-09 15:37:09 -0400

    mapping relevant JSON directly to models now
    made a static function to create a WeatherModelList containing a specific forecast, since the return of default GET is a list with only first forecast

--- src/main/java/com/accenture/weather/WeatherDAO.java
+++ src/main/java/com/accenture/weather/WeatherDAO.java
@@ -2,19 +2,12 @@ package com.accenture.weather;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
-import java.util.List;
 
 
 @Repository
@@ -37,30 +30,9 @@ public class WeatherDAO
         return webForecastSvc
                 .get()
                 .uri(ForecastsURI).retrieve()
-                .bodyToMono(String.class)
-                .map(json ->
-                {
-                    List<WeatherModel> forecasts = new LinkedList<>();
-
-                    try
-                    {
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        JsonNode nodeRoot = objectMapper.readTree(json);
-
-                        JsonNode nodePeriods = nodeRoot.get("properties").get("periods");
-                        if (nodePeriods != null && nodePeriods.isArray())
-                        {
-                            JsonNode node = nodePeriods.get(0);
-                            forecasts.add(parsePeriodToWeatherModel(node));
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.error(String.format("Error parsing JSON weather data. %s", ex.getMessage()));
-                    }
-
-                    return new WeatherModelList(forecasts.toArray(new WeatherModel[0]));
-                });
+                .bodyToMono(WeatherModelResponse.class)
+                .map(response -> response.properties)
+                .map(list -> list.ListSingleForecast(list, 0));
     }
 
     public Mono<WeatherModelList> getForecastWeek()
@@ -68,30 +40,8 @@ public class WeatherDAO
         return webForecastSvc
                 .get()
                 .uri(ForecastsURI).retrieve()
-                .bodyToMono(String.class)
-                .map(json ->
-                {
-                    List<WeatherModel> forecasts = new LinkedList<>();
-
-                    try
-                    {
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        JsonNode nodeRoot = objectMapper.readTree(json);
-
-                        JsonNode nodePeriods = nodeRoot.get("properties").get("periods");
-                        if (nodePeriods != null && nodePeriods.isArray())
-                        {
-                            for (JsonNode node : nodePeriods)
-                                forecasts.add(parsePeriodToWeatherModel(node));
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.error(String.format("Error parsing JSON weather data. %s", ex.getMessage()));
-                    }
-
-                    return new WeatherModelList(forecasts.toArray(new WeatherModel[0]));
-                });
+                .bodyToMono(WeatherModelResponse.class)
+                .map(response -> response.properties);
     }
 
     private WeatherModel parsePeriodToWeatherModel(JsonNode node)
@@ -112,8 +62,9 @@ public class WeatherDAO
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class WeatherModelResponse
+    private static class WeatherModelResponse
     {
+        @JsonProperty("properties")
         public WeatherModelList properties;
 
         public WeatherModelResponse()
